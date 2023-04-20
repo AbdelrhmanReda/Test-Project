a752ebe301298f9a32f7d4cc51956e59ade3c98b
Abdelrahman Reda <57096941+AbdelrhmanReda@users.noreply.github.com>
2023-04-20 03:26:00 +0200

    Add files via upload

---
+++ middlewares/profMW.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken"),
+  config = require("config");
+module.exports = (req, res, nxt) => {
+  // get header
+  const token = req.header("auth-token");
+
+  if (!token) res.status(401).send("Access Denied..");
+
+  const decodedToken = jwt.verify(token,config.get('jwtsec')) ; 
+  const role = decodedToken.role
+  if (!role=='prof'|| !role=='admin' ) res.send("Access Denied..") 
+  nxt();
+};
